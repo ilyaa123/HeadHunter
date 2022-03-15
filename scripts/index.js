3d1aeed3603517e47b07459686867818407ae142
Ilya Andreev <ilya_a2018@mail.ru>
2022-03-15 16:06:05 +0300

    Add optionList

--- scripts/index.js
+++ scripts/index.js
@@ -10,13 +10,27 @@ const overlayVacancy = document.querySelector('.overlay_vacancy');
 const resultList = document.querySelector('.result__list');
 const formSearch = document.querySelector('.bottom__search');
 const found = document.querySelector('.found');
+const orderBy = document.querySelector('#order_by');
+const searchPeriod = document.querySelector('#search_period');
 
+let data = [];
 
-const getData = ({search, id} = {}) => {
+const getData = ({search, id, country, city} = {}) => {
+    let url = `http://localhost:3000/api/vacancy/${id ? id : ''}`;
+
     if (search){
-        return fetch(`http://localhost:3000/api/vacancy?search=${search}`).then(response => response.json());
+        url = `http://localhost:3000/api/vacancy?search=${search}`;
     }
-    return fetch(`http://localhost:3000/api/vacancy${id ? id : ''}`).then(response => response.json());
+
+    if (city){
+        url = `http://localhost:3000/api/vacancy?city=${city}`;
+    }
+
+    if (country){
+        url = `http://localhost:3000/api/vacancy?country=${country}`;
+    }
+
+    return fetch(url).then(response => response.json());
 };
 
 
@@ -76,6 +90,24 @@ const renderCard = (data) => {
     resultList.append(...cards);
 };
 
+const sortData = () => {
+    switch (orderBy.value){
+        case 'down':
+            data.sort((a, b) => a.minCompensation > b.minCompensation ? 1 : -1);
+            break;
+        case 'up':
+            data.sort((a, b) => b.minCompensation > a.minCompensation ? 1 : -1);
+            break;
+        default: 
+            data.sort((a, b) => new Date(a.date).getTime() > new Date(b.date).getTime() ? 1 : -1);
+    }
+};
+
+const filterData = () => {
+    const date = new Date();
+    date.setDate(date.getDate() - searchPeriod.value);
+    return data.filter(item => new Date(item.date).getTime() > date); 
+};
 
 const optionHandler = () => {
     optionBtnOrder.addEventListener('click', () => {
@@ -93,6 +125,9 @@ const optionHandler = () => {
     
         if(target.classList.contains('option__item')){
             optionBtnOrder.textContent = target.textContent;
+            orderBy.value = target.dataset.sort;
+            sortData();
+            renderCard(data);
             optionListOrder.classList.remove('option__list_active');
     
             for (const elem of optionListOrder.querySelectorAll('.option__item')){
@@ -112,6 +147,9 @@ const optionHandler = () => {
     
         if(target.classList.contains('option__item')){
             optionBtnPeriod.textContent = target.textContent;
+            searchPeriod.value = target.dataset.date;
+            const tempData = filterData();
+            renderCard(tempData);
             optionListPeriod.classList.remove('option__list_active');
     
             for (const elem of optionListPeriod.querySelectorAll('.option__item')){
@@ -130,10 +168,18 @@ const cityHandler = () => {
         city.classList.toggle('city_active');
     });
     
-    cityRegionList.addEventListener('click', (event) => {
+    cityRegionList.addEventListener('click', async (event) => {
         const target = event.target;
     
         if(target.classList.contains('city__link')){
+            const hash = new URL(target.href).hash.substring(1);
+            const option = {
+                [hash]: target.textContent,
+            }
+            data = await getData(option);
+            sortData();
+            renderCard(data);
+
             topCityBtn.textContent = target.textContent;
             city.classList.remove('city_active');
         }
@@ -258,7 +304,8 @@ const searchHandler = () => {
     
         if (textSearch.length > 2){
             formSearch.search.style.borderColor = '';
-            const data = await getData({search: textSearch});
+            data = await getData({search: textSearch});
+            sortData();
             found.innerHTML = `${declOfNum(data.length, ['вакансия', 'вакансии', 'вакансий'])} &laquo; ${textSearch} &raquo;`;
     
             renderCard(data);
@@ -273,7 +320,9 @@ const searchHandler = () => {
 };
 
 const init = async () => {
-    const data = await getData();
+    data = await getData();
+    sortData();
+    data = filterData();
     renderCard(data);
     
     optionHandler();
